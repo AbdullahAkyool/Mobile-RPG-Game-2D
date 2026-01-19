e3f30b8263a5d348735e04424914526250a48f36
Abdullah Akyol <abdullah35akyol@gmail.com>
2026-01-19 18:30:41 +0300

    REFACTOR: ParticleEffectController

--- Assets/Scripts/Particle System/ParticleEffectController.cs
+++ Assets/Scripts/Particle System/ParticleEffectController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,14 +10,22 @@ public class ParticleEffectController : MonoBehaviour, IPoolable
 
     [SerializeField] private List<ParticleSystem> particleSystems;
 
+    private Coroutine returnToPoolCoroutine;
+
     public void OnSpawn()
     {
         gameObject.SetActive(true);
         PlayEffect();
+        StartReturnToPoolTimer();
     }
 
     public void OnDespawn()
     {
+        if (returnToPoolCoroutine != null)
+        {
+            StopCoroutine(returnToPoolCoroutine);
+            returnToPoolCoroutine = null;
+        }
         StopEffect();
         gameObject.SetActive(false);
     }
@@ -39,5 +48,46 @@ public class ParticleEffectController : MonoBehaviour, IPoolable
         {
             ps.Stop();
         }
+    }
+
+    private void StartReturnToPoolTimer()
+    {
+        if (returnToPoolCoroutine != null)
+        {
+            StopCoroutine(returnToPoolCoroutine);
+        }
+        returnToPoolCoroutine = StartCoroutine(ReturnToPoolAfterDuration());
+    }
+
+    private IEnumerator ReturnToPoolAfterDuration()
+    {
+        // En uzun particle duration'ı bul
+        float maxDuration = 0f;
+        if (particleSystems != null && particleSystems.Count > 0)
+        {
+            foreach (var ps in particleSystems)
+            {
+                if (ps != null)
+                {
+                    float duration = ps.main.duration + ps.main.startLifetime.constantMax;
+                    if (duration > maxDuration)
+                        maxDuration = duration;
+                }
+            }
+        }
+
+        // En az 0.5 saniye bekle
+        if (maxDuration < 0.5f)
+            maxDuration = 0.5f;
+
+        yield return new WaitForSeconds(maxDuration);
+
+        // Pool'a geri dön
+        if (PoolManager.Instance != null)
+        {
+            PoolManager.Instance.Despawn(effectPoolKey, this);
+        }
+
+        returnToPoolCoroutine = null;
     }
 }
